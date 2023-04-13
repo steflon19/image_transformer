9abd907a5827c84f77c19def7ee84524cbf99c71
steflon19 <103255619+steflon19@users.noreply.github.com>
2023-04-13 12:04:37 +0200

    fix typo in documentation

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 /// A utility function that takes in a comma separated string and returns a matrix/kernel
 /// 
-/// [`util::parse_csv`] takes a comma separated string `input_str` containing a matrix row by row
+/// [`util::parse_convolution_matrix`] takes a comma separated string `input_str` containing a matrix row by row
 /// and returns a `Vec<Vec<f32>>` containing the matrix.
 pub fn parse_convolution_matrix(input_str: &str) -> Option<Vec<Vec<f32>>> {
     let values: Vec<f32> = input_str
