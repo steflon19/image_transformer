ddd6762ea98804689be907a0dd7f21bd339aca2c
Stefan Lontschar <steflon@protonmail.com>
2023-04-16 20:48:58 +0200

    Added some testing to cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -20,3 +20,25 @@ pub struct CliArguments {
     #[structopt(parse(from_os_str), short = "o", long = "output", help = "The output destination, defaults to ./output/out with the same extension as the input file", default_value="./output/out")]
     pub output_file: PathBuf,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use structopt::StructOpt;
+    use crate::cli::CliArguments;
+
+    #[test]
+    fn test_parse_args() {
+        // Test with minimal arguments
+        let args = CliArguments::from_iter(&["test"]);
+        assert_eq!(args.input_file, PathBuf::from("./images/l.png"));
+        assert_eq!(args.matrix, String::from("-1,-1,-1,-1,8,-1,-1,-1,-1"));
+        assert_eq!(args.output_file, PathBuf::from("./output/out"));
+
+        // Test with custom arguments
+        let args = CliArguments::from_iter(&["test", "-m", "1,2,1,2,4,2,1,2,1", "-o", "./output/custom.png", "./input/custom.png"]);
+        assert_eq!(args.input_file, PathBuf::from("./input/custom.png"));
+        assert_eq!(args.matrix, String::from("1,2,1,2,4,2,1,2,1"));
+        assert_eq!(args.output_file, PathBuf::from("./output/custom.png"));
+    }
+}
